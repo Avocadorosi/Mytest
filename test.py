dac505aa945e54c3713dde341613e527a4e003e1
Alberto <albduranlopez@gmail.com>
2020-10-05 13:28:31 +0200

    product of list + test

--- test.py
+++ test.py
@@ -6,20 +6,6 @@ Created on Mon Oct  5 13:03:27 2020
 @author: ros
 
 """
-def is_prime(num):
-    if num > 1:        
-       for i in range(2, num): 
-           if (num % i) == 0: 
-               return False
-               break
-       else: 
-           return True
-    else: 
-       return False
-       
-
-
-
 
 def Suma(n): 
     sum = 0
@@ -29,5 +15,39 @@ def Suma(n):
     return sum
 
 
+def is_prime(num):
+    es = False
+    if num > 1:        
+       for i in range(2, num): 
+           if (num % i) == 0: 
+               es = False
+               break
+       else: 
+           es=True
+    else: 
+       es = False
+    return es
+       
+
+
+
+def Producto(ist) : 
+      
+    # Multiply elements one by one 
+    result = 1
+    for x in ist: 
+         result = result * x  
+    return result  
+   
+   
+# Driver code 
+list1 = [1, 22, 3]  
+list2 = [31, 2, 44] 
+print(Producto(list1)) 
+print(Producto(list2)) 
+
+
 #Maybe improve others
 is_prime(113)
+is_prime(112)
+is_prime(111)
